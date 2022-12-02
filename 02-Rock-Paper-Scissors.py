335a55aa8609953ec7f6556a201207809d5b2858
Joe Baker <joeblakeb@gmail.com>
2022-12-02 06:23:08 +0000

    Day 02

---
+++ 02-Rock-Paper-Scissors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Day 2: Rock Paper Scissors
+
+with open('inputs/02.txt') as f:
+    data = f.read().strip()
+
+score1 = 0
+score2 = 0
+pointsForChoice = {"X": 1, "Y": 2, "Z": 3}
+win = {"A": "Y", "B": "Z", "C": "X"}
+draw = {"A": "X", "B": "Y", "C": "Z"}
+lose = {"A": "Z", "B": "X", "C": "Y"}
+task2 = {"Z": (win, 6), "Y": (draw, 3), "X": (lose, 0)}
+
+for game in data.split("\n"):
+    opponent, player = game.split(" ")
+    # Task 1
+    score1 += pointsForChoice[player]
+    if player == win[opponent]:
+        score1 += 6
+    elif player == draw[opponent]:
+        score1 += 3
+    # Task 2
+    score2 += pointsForChoice[task2[player][0][opponent]] + task2[player][1]
+
+print(score1)
+print(score2)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2022
 
 My solutions for the [Advent of Code 2022](https://adventofcode.com/2022) challenges.
+
+Inputs for each task is in the `inputs` folder with as xx.txt where xx is the day number.

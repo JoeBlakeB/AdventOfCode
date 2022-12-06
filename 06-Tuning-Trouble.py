af85183337784b3bba62bc9c475b622238b45fe1
Joe Baker <joeblakeb@gmail.com>
2022-12-06 21:23:32 +0000

    Day 06: Tuning Trouble

---
+++ 06-Tuning-Trouble.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Day 6: Tuning Trouble
+
+with open("inputs/06.txt") as f:
+    data = f.read().strip()
+
+
+def lookForMarker(data, markerLength):
+    markerAfter = 0
+    for i in range(len(data) - markerLength - 1):
+        fourRecent = data[i:i+markerLength]
+        for char in fourRecent:
+            if fourRecent.count(char) != 1:
+                break
+        else:
+            markerAfter = i + markerLength
+            break
+    return markerAfter
+
+print("Packet: ", lookForMarker(data, 4))
+print("Message:", lookForMarker(data, 14))

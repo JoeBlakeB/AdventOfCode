1a88a18c65e64c5646575588002bfac1b89b4ab1
Joe Baker <joeblakeb@gmail.com>
2023-01-05 10:17:43 +0000

    Day 22: Monkey Map Part 2
    Having to make a cube out of paper is SOOOOO fun

--- 22-Monkey-Map.py
+++ 22-Monkey-Map.py
@@ -54,6 +54,54 @@ class Board:
             position = newPos(position, direction)
         return newPos(position, (0,0)), 0
 
+
+class Cube(Board):
+    wrapMappings = {
+        ((49,), range(0, 50), Board.directions[2]):
+            lambda pos: ((0, 99 + (50 - pos[1])), 2),
+        ((-1,), range(100, 150), Board.directions[2]):
+            lambda pos: ((50, 50 - (pos[1] - 99)), 2),
+
+        (range(50, 100), (-1,), Board.directions[3]):
+            lambda pos: ((0, 100 + pos[0]), 1),
+        ((-1,), range(150, 200), Board.directions[2]):
+            lambda pos: ((pos[1] - 100, 0), -1),
+
+        ((50,), range(150, 200), Board.directions[0]):
+            lambda pos: ((pos[1] - 100, 149), -1),
+        ((range(50, 100), (150,), Board.directions[1])):
+            lambda pos: ((49, 100 + pos[0]), 1),
+
+        ((150,), range(0, 50), Board.directions[0]):
+            lambda pos: ((99, 99 + (50 - pos[1])), 2),
+        ((100,), range(100, 150), Board.directions[0]):
+            lambda pos: ((149, 50 - (pos[1] - 99)), 2),
+
+        (range(100, 150), (-1,), Board.directions[3]):
+            lambda pos: ((pos[0] - 100, 199), 0),
+        (range(0, 50), (200,), Board.directions[1]):
+            lambda pos: ((100 + pos[0], 0), 0),
+
+        ((49,), range(50, 100), Board.directions[2]):
+            lambda pos: ((pos[1] - 50, 100), -1),
+        (range(0, 50), (99,), Board.directions[3]):
+            lambda pos: ((50, 50 + pos[0]), 1),
+
+        (range(100, 150), (50,), Board.directions[1]):
+            lambda pos: ((99, pos[0] - 50), 1),
+        ((100,), range(50, 100), Board.directions[0]):
+            lambda pos: ((100 + (pos[1] - 50), 49), -1)
+    }
+
+    def wrap(self, position, direction):
+        for key in self.wrapMappings:
+            if position[0] in key[0] and position[1] in key[1] and direction == key[2]:
+                return self.wrapMappings[key](position)
+        raise Exception("No wrap found for position " + str(position) + " and direction " + str(direction) + ", wrap mappings only work on the real input.")
+
+
 sequence = re.findall(r"(\d+|R|L)", sequence)
 
 print("Board password:", Board(mapData).getPassword(sequence))
+
+print("Cube password:", Cube(mapData).getPassword(sequence))

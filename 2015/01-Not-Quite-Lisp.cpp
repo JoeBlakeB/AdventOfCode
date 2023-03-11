50fb42ed813672032cdaa0476f250e153a4dd23c
Joe Baker <joeblakeb@gmail.com>
2023-03-11 10:02:13 +0000

    Start 2015 in c++

---
+++ 2015/01-Not-Quite-Lisp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::string input;
+    std::cin >> input;
+
+    int floorNum = 0;
+    int firstReachBasement = 0;
+    for (long unsigned int i = 0; i < input.length(); i++) {
+        if (input[i] == '(') {
+            floorNum++;
+        } else if (input[i] == ')') {
+            floorNum--;
+        }
+
+        if (floorNum < 0 && !firstReachBasement) {
+            firstReachBasement = i + 1;
+        }
+    }
+
+    std::cout << "You are on floor # " << floorNum << std::endl;
+    std::cout << "You first reach the basement at position # " << firstReachBasement << std::endl;
+    return 0;
+}
---
+++ 2015/README.md
@@ -0,0 +1,17 @@
+# Advent of Code 2015
+
+My solutions for the [Advent of Code 2015](https://adventofcode.com/2015) challenges done to learn C++.
+
+## Usage
+
+Compile a solution with this command:
+
+```bash
+g++ -std=c++17 -Wall -Wextra -Wpedantic -Werror -O3 -o bin/solution solution.cpp
+```
+
+Run a solution with this command:
+
+```bash
+./bin/solution < input.txt
+```

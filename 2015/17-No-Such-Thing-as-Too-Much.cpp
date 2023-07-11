cee421bde5d4b80e7934d73a3b9b017d8dc183eb
Joe Baker <joeblakeb@gmail.com>
2023-07-11 10:09:56 +0100

    2015-17: No Such Thing as Too Much

---
+++ 2015/17-No-Such-Thing-as-Too-Much.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr unsigned int LITRES = 150;
+
+vector<unsigned int> getInputNumbersSortedVector() {
+    vector<unsigned int> inputNumbers;
+    string line;
+    while (getline(cin, line)) {
+        inputNumbers.push_back(stoi(line));
+    }
+    sort(inputNumbers.rbegin(), inputNumbers.rend());
+    return inputNumbers;
+}
+
+pair<unsigned int, unsigned int> getPossibleCombinationCount(
+    vector<unsigned int>& containerSizes,
+    unsigned int targetSum
+) {
+    unsigned int totalPossibleCombinations = 0;
+    unsigned int currentSum = containerSizes[0];
+    stack<unsigned int> countedIndexes;
+    countedIndexes.push(0);
+    unsigned int i = 0;
+
+    unsigned int lowestNumberOfContainers = 0xffffffff;
+    unsigned int lowestNumberOfContainersCount = 0;
+
+    while (!countedIndexes.empty()) {
+        unsigned int newSum = currentSum + containerSizes[++i];
+        if (newSum == targetSum) {
+            totalPossibleCombinations++;
+
+            if (countedIndexes.size() + 1 < lowestNumberOfContainers) {
+                lowestNumberOfContainers = countedIndexes.size() + 1;
+                lowestNumberOfContainersCount = 1;
+            } else if (countedIndexes.size() + 1 == lowestNumberOfContainers) {
+                lowestNumberOfContainersCount++;
+            }
+        } else if (newSum < targetSum && i < containerSizes.size() - 1) {
+            countedIndexes.push(i);
+            currentSum = newSum;
+        }
+
+        if (i >= containerSizes.size() - 1) {
+            i = countedIndexes.top();
+            countedIndexes.pop();
+            currentSum -= containerSizes[i];
+
+            if (currentSum == 0 && i < containerSizes.size() - 2) {
+                currentSum = containerSizes[++i];
+                countedIndexes.push(i);
+            }
+        }
+    }
+
+    return {totalPossibleCombinations, lowestNumberOfContainersCount};
+}
+
+int main() {
+    vector<unsigned int> containerSizes = getInputNumbersSortedVector();
+    
+    pair<int, int> possibleCombinations = getPossibleCombinationCount(containerSizes, LITRES);
+
+    cout << "Total combinations of containers for " << LITRES << " liters: "
+         << possibleCombinations.first << endl;
+
+    cout << "Lowest number of containers combinations: "
+         << possibleCombinations.second << endl;
+
+    return 0;
+}

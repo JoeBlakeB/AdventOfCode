09fe1d1073e5199c1526043a8eb8f4b36f15ed4c
Joe Baker <joeblakeb@gmail.com>
2025-12-09 09:54:19 +0000

    realised i dont even need to specify the edges seperately, can just do polygon covers polygon

--- 2025/09-Movie-Theater.py
+++ 2025/09-Movie-Theater.py
@@ -6,7 +6,7 @@
 
 import sys
 from itertools import combinations
-from shapely.geometry import Polygon, LineString
+from shapely.geometry import Polygon
 
 RED_TILES = [(int(tile.split(",")[0]), int(tile.split(",")[1])) for tile in iter(lambda: next(sys.stdin).rstrip("\n"), "")]
 POSSIBLE_RECTANGLES = [(min(a, x), min(b, y), max(a, x), max(b, y))
@@ -20,8 +20,5 @@ polygon = Polygon(RED_TILES)
 
 print("largest area with red and green tiles:", max([
     ((x - a + 1) * (y - b + 1)) for a, b, x, y in POSSIBLE_RECTANGLES
-    if polygon.covers(LineString([(a, b), (x, b)])) and
-       polygon.covers(LineString([(x, b), (x, y)])) and
-       polygon.covers(LineString([(a, y), (x, y)])) and
-       polygon.covers(LineString([(a, b), (a, y)]))
+    if polygon.covers(Polygon([(a, b), (x, b), (x, y), (a, y)]))
 ]))
